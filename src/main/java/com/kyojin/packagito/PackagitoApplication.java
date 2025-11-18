d725fc9c92f72a058320867ba36062bc878409bd
tahajaiti <taha.jaiti@gmail.com>
2025-11-18 17:00:44 +0100

    INIT: PAC-8 initial project files

---
+++ src/main/java/com/kyojin/packagito/PackagitoApplication.java
@@ -0,0 +1,13 @@
+package com.kyojin.packagito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyojin/packagito/PackagitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyojin.packagito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
