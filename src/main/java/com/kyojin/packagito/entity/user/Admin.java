b7f1bf04c868a5031a5bcaded388241fb81c9973
tahajaiti <taha.jaiti@gmail.com>
2025-11-20 16:16:34 +0100

    FEAT: PAC-17 created the admin entity

---
+++ src/main/java/com/kyojin/packagito/entity/user/Admin.java
@@ -0,0 +1,16 @@
+package com.kyojin.packagito.entity.user;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.TypeAlias;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@TypeAlias("admin")
+public class Admin extends User{
+}
