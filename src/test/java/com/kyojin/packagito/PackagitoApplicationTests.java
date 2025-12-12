b980cd2c3a1dce7506a0f81df67e8657ceb4aa51
tahajaiti <taha.jaiti@gmail.com>
2025-12-12 11:41:42 +0100

    FIX: fixed user bean in spring test

--- src/test/java/com/kyojin/packagito/PackagitoApplicationTests.java
+++ src/test/java/com/kyojin/packagito/PackagitoApplicationTests.java
@@ -1,12 +1,17 @@
 package com.kyojin.packagito;
 
+import com.kyojin.packagito.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("ci")
 class PackagitoApplicationTests {
+
+    @MockitoBean
+    private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
