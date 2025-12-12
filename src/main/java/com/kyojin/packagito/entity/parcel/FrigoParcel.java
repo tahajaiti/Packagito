1db4777cfb9e51d9fa9d7c672a959908b6e10d39
tahajaiti <taha.jaiti@gmail.com>
2025-12-12 16:45:41 +0100

    FEAT: PAC-29 implemented the parcel types with default statsu for each type #done

---
+++ src/main/java/com/kyojin/packagito/entity/parcel/FrigoParcel.java
@@ -0,0 +1,28 @@
+package com.kyojin.packagito.entity.parcel;
+
+import com.kyojin.packagito.entity.enums.ParcelType;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.TypeAlias;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Document(collection = "parcels")
+@TypeAlias("frigo")
+public class FrigoParcel extends Parcel {
+
+    @Indexed
+    @Builder.Default
+    private ParcelType type = ParcelType.FRIGO;
+
+
+    private Double temperatureMin;
+
+    private Double temperatureMax;
+}
---
+++ src/main/java/com/kyojin/packagito/entity/parcel/StandardParcel.java
@@ -0,0 +1,24 @@
+package com.kyojin.packagito.entity.parcel;
+
+import com.kyojin.packagito.entity.enums.ParcelType;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.TypeAlias;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Document(collection = "parcels")
+@TypeAlias("standard")
+public class StandardParcel extends Parcel {
+
+    @Indexed
+    @Builder.Default
+    private ParcelType type = ParcelType.STANDARD;
+
+}
