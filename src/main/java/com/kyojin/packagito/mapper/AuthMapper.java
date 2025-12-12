bae59252593a9446bf4784277e5acf77c1a8b82f
tahajaiti <taha.jaiti@gmail.com>
2025-12-12 12:26:18 +0100

    FEAT: PAC-7 add auth mapper for mapping

---
+++ src/main/java/com/kyojin/packagito/mapper/AuthMapper.java
@@ -0,0 +1,40 @@
+package com.kyojin.packagito.mapper;
+
+
+import com.kyojin.packagito.dto.request.RegisterRequest;
+import com.kyojin.packagito.dto.response.AuthResponse;
+import com.kyojin.packagito.entity.enums.CarrierStatus;
+import com.kyojin.packagito.entity.enums.Role;
+import com.kyojin.packagito.entity.user.Carrier;
+import com.kyojin.packagito.entity.user.User;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", imports = {Role.class, CarrierStatus.class})
+public interface AuthMapper {
+
+    @Mapping(target = "type", constant = "Bearer")
+    @Mapping(target = "role", source = "user.role")
+    @Mapping(target = "username", source = "user.username")
+    @Mapping(target = "token", source = "token")
+    AuthResponse toAuthResponse(User user, String token);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", expression = "java(Role.TRANSPORTER)")
+    @Mapping(target = "active", constant = "true")
+    @Mapping(target = "status", expression = "java(CarrierStatus.AVAILABLE)")
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Carrier toCarrier(RegisterRequest request);
+
+
+    @Named("extractStatus")
+    default CarrierStatus extractStatus(User user) {
+        return user instanceof Carrier carrier ? carrier.getStatus() : null;
+    }
+
+    @Named("extractSpecialty")
+    default com.kyojin.packagito.entity.enums.Specialty extractSpecialty(User user) {
+        return user instanceof Carrier carrier ? carrier.getSpecialty() : null;
+    }
+}
